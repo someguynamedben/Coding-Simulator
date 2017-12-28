194a1d198f52a103a92b773ae59070550ec43e45
someguynamedben <someguynamedben7@gmail.com>
2017-12-28 16:44:42 -0700

    Cleaned up unused code/imports in Main.java

--- src/CodingSim/Main.java
+++ src/CodingSim/Main.java
@@ -1,8 +1,6 @@
 package CodingSim;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,16 +8,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        ToggleSwitch toggleSwitch = new ToggleSwitch();
         Controller controller = new Controller();
-        //Parent root = FXMLLoader.load(getClass().getResource("switches.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle(null);
         primaryStage.setScene(new Scene(controller));
-        //primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
+    
     public static void main(String[] args) {
         launch(args);
     }
